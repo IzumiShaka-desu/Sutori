d761430e05b4aee2c047c2bda700747b20bcc21b
IzumiShaka-desu <shakaaji29@gmail.com>
2022-04-23 09:27:29 +0700

    setup activity

---
+++ app/src/main/java/com/darkshandev/sutori/presentation/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.darkshandev.sutori.presentation
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.darkshandev.sutori.R
+import com.darkshandev.sutori.presentation.viewmodels.StoryViewModel
+import com.darkshandev.sutori.presentation.viewmodels.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private val storyViewModel: StoryViewModel by viewModels()
+    private val userViewModel: UserViewModel by viewModels()
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA, Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private const val MY_PERMISSIONS_REQUEST_LOCATION = 99
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            splashScreenViewProvider.iconView
+                .animate()
+                .setDuration(
+                    300
+                ).translationY(-0f)
+                .alpha(0f)
+                .withEndAction {
+                    // After the fade out, remove the
+                    // splash and set content view
+                    splashScreenViewProvider.remove()
+                }.start()
+        }
+        setContentView(R.layout.activity_main)
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+}
