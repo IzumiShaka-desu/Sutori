dc3038525dc4286888ecd1628ab7fb9dde00f287
IzumiShaka-desu <shakaaji29@gmail.com>
2022-04-23 09:30:41 +0700

    create network result class

---
+++ app/src/main/java/com/darkshandev/sutori/data/models/NetworkResult.kt
@@ -0,0 +1,11 @@
+package com.darkshandev.sutori.data.models
+
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : NetworkResult<T>(data)
+    class Error<T>(message: String, data: T? = null) : NetworkResult<T>(data, message)
+    class Loading<T> : NetworkResult<T>()
+    class Initial<T> : NetworkResult<T>()
+}
