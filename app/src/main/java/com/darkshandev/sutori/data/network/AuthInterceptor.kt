6df631cf1a9425ebb43e7751109ee03ceca43c26
IzumiShaka-desu <shakaaji29@gmail.com>
2022-04-23 09:31:59 +0700

    add auth interceptor

---
+++ app/src/main/java/com/darkshandev/sutori/data/network/AuthInterceptor.kt
@@ -0,0 +1,20 @@
+package com.darkshandev.sutori.data.network
+
+import com.darkshandev.sutori.data.datasources.SessionService
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+
+
+class AuthInterceptor(private val sessionService: SessionService) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val req = chain.request()
+        val basicReqBuilder = req.newBuilder()
+        var token: String? = null
+        runBlocking { token = sessionService.getUser().first()?.token }
+        if (token != null) basicReqBuilder.addHeader("Authorization", "Bearer $token")
+        val basicReq = basicReqBuilder.build()
+        return chain.proceed(basicReq)
+    }
+}
