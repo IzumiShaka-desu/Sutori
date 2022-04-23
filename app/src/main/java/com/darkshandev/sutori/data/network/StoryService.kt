9d30d33c4ac7734d5c626341ca4ca94f3ee47834
IzumiShaka-desu <shakaaji29@gmail.com>
2022-04-23 10:20:06 +0700

    ✨feature: apply pagination on getstory

--- app/src/main/java/com/darkshandev/sutori/data/network/StoryService.kt
+++ app/src/main/java/com/darkshandev/sutori/data/network/StoryService.kt
@@ -5,14 +5,15 @@ import com.darkshandev.sutori.data.models.StoriesResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Response
-import retrofit2.http.GET
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
+import retrofit2.http.*
 
 interface StoryService {
     @GET("stories")
-    suspend fun getStory(): Response<StoriesResponse>
+    suspend fun getStory(
+        @Query("page") page: Int? = null,
+        @Query("size") size: Int? = null,
+        @Query("location") isLocationAvailable: Int? = null
+    ): Response<StoriesResponse>
 
     @Multipart
     @POST("stories")
@@ -22,5 +23,4 @@ interface StoryService {
         @Part("lat") lat: RequestBody?,
         @Part("lon") lon: RequestBody?,
     ): Response<PostResponse>
-
 }
