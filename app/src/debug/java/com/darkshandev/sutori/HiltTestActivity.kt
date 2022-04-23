6c72973ec493647f436c1c4ee12c6435c052f729
IzumiShaka-desu <shakaaji29@gmail.com>
2022-04-23 10:11:37 +0700

    add debug

---
+++ app/src/debug/java/com/darkshandev/sutori/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.darkshandev.sutori
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
