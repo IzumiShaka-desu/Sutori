3145cb029dc644b882d99d43efd8ba23f3a88783
IzumiShaka-desu <shakaaji29@gmail.com>
2022-04-23 07:50:34 +0700

    init

---
+++ app/src/main/java/com/darkshandev/sutori/app/App.kt
@@ -0,0 +1,7 @@
+package com.darkshandev.sutori.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
---
+++ app/src/main/java/com/darkshandev/sutori/app/Config.kt
@@ -0,0 +1,6 @@
+package com.darkshandev.sutori.app
+
+object Config {
+    const val BASE_URL: String = "https://story-api.dicoding.dev/v1/"
+    const val maxAgeTokenInSecond = 3600
+}
