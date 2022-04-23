3ee234c638782d06a60a316a6795d935a4971556
IzumiShaka-desu <shakaaji29@gmail.com>
2022-04-23 09:30:26 +0700

    add register model

---
+++ app/src/main/java/com/darkshandev/sutori/data/models/PostResponse.kt
@@ -0,0 +1,3 @@
+package com.darkshandev.sutori.data.models
+
+data class PostResponse(val error: Boolean, val message: String)
---
+++ app/src/main/java/com/darkshandev/sutori/data/models/request/RegisterRequest.kt
@@ -0,0 +1,3 @@
+package com.darkshandev.sutori.data.models.request
+
+data class RegisterRequest(val name: String, val email: String, val password: String)
