58406c64a27e8fba98a07dde0caf29aa78494f2b
IzumiShaka-desu <shakaaji29@gmail.com>
2022-04-23 10:31:51 +0700

    create pager adapter

---
+++ app/src/main/java/com/darkshandev/sutori/presentation/adapter/PagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.darkshandev.sutori.presentation.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class PagerAdapter(
+    private val fragments: List<Fragment>,
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+
+
+    override fun getItemCount(): Int = fragments.size
+
+    override fun createFragment(position: Int): Fragment = fragments[position]
+}
