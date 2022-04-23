23b5506eac751a063c9b55b7c0772e9b11b3ed23
IzumiShaka-desu <shakaaji29@gmail.com>
2022-04-23 09:35:46 +0700

    add story service

---
+++ app/src/main/java/com/darkshandev/sutori/data/network/StoryService.kt
@@ -0,0 +1,26 @@
+package com.darkshandev.sutori.data.network
+
+import com.darkshandev.sutori.data.models.PostResponse
+import com.darkshandev.sutori.data.models.StoriesResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface StoryService {
+    @GET("stories")
+    suspend fun getStory(): Response<StoriesResponse>
+
+    @Multipart
+    @POST("stories")
+    suspend fun postStory(
+        @Part photo: MultipartBody.Part,
+        @Part("description") description: RequestBody,
+        @Part("lat") lat: RequestBody?,
+        @Part("lon") lon: RequestBody?,
+    ): Response<PostResponse>
+
+}
